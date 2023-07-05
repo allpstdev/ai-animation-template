196e69439c2e33faf66d650c7cba6d05e6bde34d
allpstdev <tarang@allps.ch>
2023-07-05 21:33:30 +0530

    feat: completed technology page with all content

---
+++ app/technology/page.js
@@ -0,0 +1,70 @@
+import Image from 'next/image'
+
+import company_values from '../../public/company_values.svg'
+import demo from '../../public/Fundamentals_logo_lg.png'
+import profile_gunther from '../../public/profile_gunther.png'
+import profile_mario from '../../public/profile_mario.png'
+import profile_mike from '../../public/profile_mike.png'
+
+const TechnologyPage = () => {
+    return (
+        <div>
+            <h1 className="m-10 text-center text-4xl font-bold tracking-widest sm:text-7xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-9xl">
+                Technology
+            </h1>
+            <h2 className="mb-24 text-center text-2xl font-medium tracking-widest sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-7xl">
+                Uncovering the black box
+            </h2>
+
+            <h4 className="mb-2 px-44 text-center text-xl font-light tracking-[-0.0001em] sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl 2xl:text-5xl">
+                {`We build forward-thinking AI technology and make it available in user-centric apps. `}
+            </h4>
+            {/* <div className='min-w-sm h-full'> */}
+            <Image
+                alt="demo"
+                src={demo}
+                width="0"
+                height="0"
+                className='w-full lg:w-1/2 mx-auto lg:mx-auto mb-72 mt-20 invisible'
+            />
+            <section className="bg-[#FDF9F5] px-44 pb-12 pt-24">
+                <div className="mt-32 pb-96">
+                    <div>
+                        <h3 className="mt-32 mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                            Research
+                        </h3>
+                        <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                            {`We build AI tools, tailor-made for those who put design first - in whatever they do. Our goal is not to replace humans in the visual space but to enhance their creative output with Artificial Intelligence. Technological augmentation of digital art will start creating content that will take our breaths away. All Fundamentals products are trained to understand the personal artistic handwriting of each user. As it learns, our AI starts enhancing work by automating repetitive tasks and suggesting visual elements - in each user's very own style.`}
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="mt-32 mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                            Proprietary Technology
+                        </h3>
+                        <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                            {`We build AI tools, tailor-made for those who put design first - in whatever they do. Our goal is not to replace humans in the visual space but to enhance their creative output with Artificial Intelligence. Technological augmentation of digital art will start creating content that will take our breaths away. All Fundamentals products are trained to understand the personal artistic handwriting of each user. As it learns, our AI starts enhancing work by automating repetitive tasks and suggesting visual elements - in each user's very own style. `}
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="mt-32 mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                            Software Development
+                        </h3>
+                        <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                            {`We build AI tools, tailor-made for those who put design first - in whatever they do. Our goal is not to replace humans in the visual space but to enhance their creative output with Artificial Intelligence. Technological augmentation of digital art will start creating content that will take our breaths away. All Fundamentals products are trained to understand the personal artistic handwriting of each user. As it learns, our AI starts enhancing work by automating repetitive tasks and suggesting visual elements - in each user's very own style.`}
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="mt-32 mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                            Design - our principle
+                        </h3>
+                        <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                            {`We build AI tools, tailor-made for those who put design first - in whatever they do. Our goal is not to replace humans in the visual space but to enhance their creative output with Artificial Intelligence. Technological augmentation of digital art will start creating content that will take our breaths away. All Fundamentals products are trained to understand the personal artistic handwriting of each user. As it learns, our AI starts enhancing work by automating repetitive tasks and suggesting visual elements - in each user's very own style.`}
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default TechnologyPage
