ca66fa6678a2bb71fae740648a1539ca29a296e5
allpstdev <tarang@allps.ch>
2023-07-05 19:29:52 +0530

    feat: completed about page with all content

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,150 @@
-import React from 'react';
+import Image from 'next/image'
 
-function about() {
-    return <div className="my-24 text-teal-700">about</div>;
+import company_values from '../../public/company_values.svg'
+import demo from '../../public/Fundamentals_logo_lg.png'
+import profile_gunther from '../../public/profile_gunther.png'
+import profile_mario from '../../public/profile_mario.png'
+import profile_mike from '../../public/profile_mike.png'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className="m-10 text-center text-4xl font-bold tracking-widest sm:text-7xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-9xl">
+                About
+            </h1>
+            <h2 className="mb-24 text-center text-2xl font-medium tracking-widest sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-7xl">
+                Fundamentals Artificial Intelligence
+            </h2>
+
+            <h4 className="mb-2 px-44 text-center text-xl font-light tracking-[-0.0001em] sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl 2xl:text-5xl">
+                {`Our founding team uses 100+ years combined experience in media, tech, and corporate finance to create AI excellence`}
+            </h4>
+            {/* <div className='min-w-sm h-full'> */}
+            <Image
+                alt="demo"
+                src={demo}
+                width="0"
+                height="0"
+                className='w-full lg:w-1/2 mx-auto lg:mx-auto mb-72 mt-20'
+            />
+            <section className="bg-[#FDF9F5] px-44 pb-12 pt-24">
+                <div className="mt-32">
+                    <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                        Our mission
+                    </h3>
+                    <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                        {`We build AI tools, tailor-made for those who put design first - in whatever they do. Our goal is not to replace humans in the visual space but to enhance their creative output with Artificial Intelligence. Technological augmentation of digital art will start creating content that will take our breaths away. All Fundamentals products are trained to understand the personal artistic handwriting of each user. As it learns, our AI starts enhancing work by automating repetitive tasks and suggesting visual elements - in each user's very own style.`}
+                    </p>
+                </div>
+                <div className="mt-36">
+                    <Image
+                        alt="company values"
+                        width={0}
+                        height={0}
+                        className="w-100vw h-auto"
+                        src={company_values}
+                    />
+                </div>
+
+                <div className="mt-32">
+                    <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                        Our team
+                    </h3>
+                    <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                        {`Fundamentals is a tech firm connecting worlds. Our people make it possible, each contributing with their very own skills and experiences. We are a diverse team located in the tech hub of Zug, Switzerland with talent from all parts of the world. We are media creatives, coders, award-winning writers, investment bankers, real estate professionals, consultants, race drivers, digital artists, and professional poker players.Together we forge the Fundamentals team.`}
+                    </p>
+                    <div className="mt-48 -mb-[2px] ml-44 h-[1px] w-4/5 bg-black" />
+                    <div className=" flex flex-col xl:flex-row">
+                        <Image
+                            alt="Mairo Wiedl profile picture"
+                            width={0}
+                            height={0}
+                            className="h-auto w-80"
+                            src={profile_mario}
+                        />
+                        <div className="w-full flex flex-col justify-center border-b border-black pl-7">
+                            <div className="mb-4 text-7xl font-normal tracking-widest">
+                                Mario Wiedl
+                            </div>
+                            <div className="text-4xl font-light tracking-default text-black/[0.8]">
+                                Co-Founder and CEO
+                            </div>
+                        </div>
+                    </div>
+                    <div className="py-12">
+                        <div className="text-center  text-4xl font-normal leading-[66px] tracking-widest text-black">
+                            5+ years experience in corporate finance, building
+                            tech start-ups and building / managing development
+                            teams.
+                        </div>
+                        <div className="mt-10 text-center text-2xl  font-normal leading-[66px]  tracking-widest text-black">
+                            “ Imagine anyone could create the same high-end
+                            content. No design teams, up to 90% faster than
+                            before, no complex tools. All you need is your
+                            imagination. We would change the game! Entertainment
+                            would be all about creativity — not big budgets.
+                            That’s what I want to build. “
+                        </div>
+                    </div>
+                    <br />
+
+                    <div className="-mb-[3px] ml-44 h-[1px] w-4/5 bg-black" />
+                    <div className=" flex flex-col xl:flex-row">
+                        <Image
+                            alt="Mike Milligan profile picture"
+                            width={0}
+                            height={0}
+                            className="h-auto w-80"
+                            src={profile_mike}
+                        />
+                        <div className="flex w-full flex-col justify-center border-b border-black pl-7">
+                            <div className="mb-4 text-7xl font-normal tracking-widest">
+                                Mike Milligan
+                            </div>
+                            <div className="text-4xl font-light tracking-default text-black/[0.8]">
+                                Co-Founder and CTO
+                            </div>
+                        </div>
+                    </div>
+                    <div className="py-12 px-5 mt-2">
+                        <div className="text-center  text-4xl font-normal leading-[66px] tracking-widest text-black">
+                            20+ years experience in animation, media production, script writing, web design, and software development.
+                        </div>
+                        <div className="mt-10 text-center text-2xl font-normal leading-[66px] tracking-widest text-black">
+                            “ I want to build a useful integration of AI into the highly manual and inefficient workflows of traditional media production and design. Generative AI allows designers to focus on their visuals rather than fighting with complex tools. That’s the goal I ultimately want to achieve. “
+                        </div>
+                    </div>
+
+                    <div className="-mb-[4px] ml-44 h-[1px] w-4/5 bg-black" />
+                    <div className=" flex flex-col xl:flex-row">
+                        <Image
+                            alt="Günther Lüttecke profile picture"
+                            width={0}
+                            height={0}
+                            className="h-auto w-80 rounded-full bg-white"
+                            src={profile_gunther}
+                        />
+                        <div className="flex w-full flex-col justify-center border-b border-black pl-7">
+                            <div className="mb-3 text-7xl font-normal tracking-widest">
+                                Günther Lüttecke
+                            </div>
+                            <div className="text-4xl font-light tracking-default text-black/[0.8]">
+                                President of the board of directors
+                            </div>
+                        </div>
+                    </div>
+                    <div className="py-10 mt-2">
+                        <div className="text-center  text-4xl font-normal leading-[66px] tracking-widest text-black">
+                            50+ years experience in project management, real estate development, building teams and business strategies.
+                        </div>
+                        <div className="mt-10 text-center text-2xl  font-normal leading-[66px] tracking-widest text-black">
+                            MISSING!
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
-export default about;
+export default AboutPage
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import { MyComponent } from '@/components/Swiper';
+import { MyComponent } from '@/components/Swiper'
 /* eslint-disable react/no-unescaped-entities */
-import Image from 'next/image';
-import React from 'react';
+import Image from 'next/image'
+import React from 'react'
 
 import demo from '../public/demo.png';
 import image1 from '../public/image1.png';
@@ -32,7 +32,7 @@ const HomePage = () => {
                 sizes="100vw"
                 className="w-100vw mx-auto my-12 h-auto"
             />
-            <section className="bg-[#FDF9F5D9] pb-12 px-44 pt-24">
+            <section className="bg-[#FDF9F5] pb-12 px-44 pt-24">
                 <div className="grid grid-cols-1 gap-3 lg:grid-cols-4 lg:gap-4">
                     <div className="flex flex-1 flex-col p-4 place-items-center gap-3 rounded-3xl border-2 border-black">
                         <Image
@@ -62,31 +62,50 @@ const HomePage = () => {
                             height="0"
                             className='w-52 h-3/4'
                         />
-                        <p className="text-center text-3xl font-normal">Augmentation of creative workflows</p>
+                        <p className="text-center text-3xl font-normal">
+                            Augmentation of creative workflows
+                        </p>
                     </div>
-                    <div className="flex flex-1 flex-col p-4 place-items-center gap-3 rounded-3xl border-2 border-black">
+                    <div className="flex flex-1 flex-col place-items-center gap-3 rounded-3xl border-2 border-black p-4">
                         <Image
                             alt="Make AI available to users image"
                             src={image4}
                             width="0"
                             height="0"
-                            className='w-52 h-3/4'
+                            className="h-3/4 w-52"
                         />
-                        <p className="text-center text-3xl font-normal">Make AI available to users</p>
+                        <p className="text-center text-3xl font-normal">
+                            Make AI available to users
+                        </p>
                     </div>
                 </div>
-                <div className='mt-32'>
+                <div className="mt-32">
                     <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
                         About
                     </h3>
-                    <p className='text-3xl leading-[2.25em] font-normal text-justify'>Fundamentals Artificial Intelligence AG is a future-oriented tech company devoted to identifying, researching, and developing software products enhancing design-focused workflows using AI and related technologies. We are committed to contributing to a safe and controlled global AI space. Our focus is on intuitive, user-centric tools for artists, designers, media producers, and creators to step up their game using cutting-edge technology.  </p>
+                    <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                        Fundamentals Artificial Intelligence AG is a
+                        future-oriented tech company devoted to identifying,
+                        researching, and developing software products enhancing
+                        design-focused workflows using AI and related
+                        technologies. We are committed to contributing to a safe
+                        and controlled global AI space. Our focus is on
+                        intuitive, user-centric tools for artists, designers,
+                        media producers, and creators to step up their game
+                        using cutting-edge technology.{' '}
+                    </p>
                 </div>
-                <div className='mt-36'>
+                <div className="mt-36">
                     <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
                         Try creating videos with AI
                     </h3>
-                    <p className='text-3xl leading-[2.25em] font-normal text-justify'>
-                        Motion Generative Artificial Intelligence for everyone? <span className='font-bold'>Here you go!</span> Try our AI video generator for free. Get yourself a Fundamentals log-in and start creating beautiful animated content based on simple text prompts. You receive all rights for your content.
+                    <p className="text-justify text-3xl font-normal leading-[2.25em]">
+                        Motion Generative Artificial Intelligence for everyone?{' '}
+                        <span className="font-bold">Here you go!</span> Try our
+                        AI video generator for free. Get yourself a Fundamentals
+                        log-in and start creating beautiful animated content
+                        based on simple text prompts. You receive all rights for
+                        your content.
                     </p>
                 </div>
                 <Image
@@ -95,43 +114,59 @@ const HomePage = () => {
                     width="0"
                     height="0"
                     sizes="100vw"
-                    className="w-full my-12 h-auto"
+                    className="my-12 h-auto w-full"
                 />
-                <div className='mt-32'>
+                <div className="mt-32">
                     <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
                         Topics
                     </h3>
-                    <div className="flex flex-col items-center justify-center w-full gap-6">
-                        <div className="w-full border-2 border-black flex items-center justify-center rounded-[24px] h-96 bg-[url('../public/topic_1.png')] bg-cover bg-no-repeat">
-                            <p className="text-center tracking-widest text-7xl">MediaTech</p>
+                    <div className="flex w-full flex-col items-center justify-center gap-6">
+                        <div className="flex h-96 w-full items-center justify-center rounded-[24px] border-2 border-black bg-[url('../public/topic_1.png')] bg-cover bg-no-repeat">
+                            <p className="text-center text-7xl tracking-widest">
+                                MediaTech
+                            </p>
                         </div>
-                        <div className="w-full border-2 border-black flex items-center justify-center rounded-[24px] h-96 bg-[url('../public/topic_2.png')] bg-cover bg-no-repeat">
-                            <p className="text-center tracking-widest text-7xl">Real Estate</p>
+                        <div className="flex h-96 w-full items-center justify-center rounded-[24px] border-2 border-black bg-[url('../public/topic_2.png')] bg-cover bg-no-repeat">
+                            <p className="text-center text-7xl tracking-widest">
+                                Real Estate
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className='mt-32'>
+                <div className="mt-32">
                     <h3 className="mb-12 text-left text-2xl font-light tracking-[0.25em] sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
                         News
                     </h3>
-                    <div className="grid grid-cols-1 gap-3 xl:grid-cols-3 xl:gap-6 h-[700px]">
-                        <div className="flex flex-1 flex-col p-4 place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white">
-                            <div className='w-full h-2/5 border-[3px] border-black rounded-2xl bg-[#FDF9F5D9]'>
-
-                            </div>
-                            <p className="text-justify text-xl leading-10 h-3/5 font-inter">Integrate generative AI into your animation workflow, decrease production time and budget and be in control of the creative process of your project like never before. With Fundamentals Motion Maker.</p>
+                    <div className="grid h-[700px] grid-cols-1 gap-3 xl:grid-cols-3 xl:gap-6">
+                        <div className="flex flex-1 flex-col place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white p-4">
+                            <div className="h-2/5 w-full rounded-2xl border-[3px] border-black bg-[#FDF9F5D9]"></div>
+                            <p className="font-inter h-3/5 text-justify text-xl leading-10">
+                                Integrate generative AI into your animation
+                                workflow, decrease production time and budget
+                                and be in control of the creative process of
+                                your project like never before. With
+                                Fundamentals Motion Maker.
+                            </p>
                         </div>
-                        <div className="flex flex-1 flex-col p-4 place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white">
-                            <div className='w-full h-2/5 border-[3px] border-black rounded-2xl bg-[#FDF9F5D9]'>
-
-                            </div>
-                            <p className="text-justify text-xl leading-10 h-3/5 font-inter">Integrate generative AI into your animation workflow, decrease production time and budget and be in control of the creative process of your project like never before. With Fundamentals Motion Maker.</p>
+                        <div className="flex flex-1 flex-col place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white p-4">
+                            <div className="h-2/5 w-full rounded-2xl border-[3px] border-black bg-[#FDF9F5D9]"></div>
+                            <p className="font-inter h-3/5 text-justify text-xl leading-10">
+                                Integrate generative AI into your animation
+                                workflow, decrease production time and budget
+                                and be in control of the creative process of
+                                your project like never before. With
+                                Fundamentals Motion Maker.
+                            </p>
                         </div>
-                        <div className="flex flex-1 flex-col p-4 place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white">
-                            <div className='w-full h-2/5 border-[3px] border-black rounded-2xl bg-[#FDF9F5D9]'>
-
-                            </div>
-                            <p className="text-justify text-xl leading-10 h-3/5 font-inter">Integrate generative AI into your animation workflow, decrease production time and budget and be in control of the creative process of your project like never before. With Fundamentals Motion Maker.</p>
+                        <div className="flex flex-1 flex-col place-items-center gap-3 rounded-3xl border-[3px] border-black bg-white p-4">
+                            <div className="h-2/5 w-full rounded-2xl border-[3px] border-black bg-[#FDF9F5D9]"></div>
+                            <p className="font-inter h-3/5 text-justify text-xl leading-10">
+                                Integrate generative AI into your animation
+                                workflow, decrease production time and budget
+                                and be in control of the creative process of
+                                your project like never before. With
+                                Fundamentals Motion Maker.
+                            </p>
                         </div>
                     </div>
                     <div className='flex justify-end mt-12 gap-10'>
