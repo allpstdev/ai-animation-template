af14300bd0cdaa329632fdc25d10590facaac8e9
allpstdev <tarang@allps.ch>
2023-07-17 15:46:50 +0530

    chore: cleaning config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,7 @@ const nextConfig = {
         styledComponents: {
             displayName: false,
         },
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
-    },
+    }
 }
 
 module.exports = nextConfig
